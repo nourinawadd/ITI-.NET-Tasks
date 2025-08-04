34959302144a288b090145ad0e9d7dd02335b7e6
nourinawadd <nourinawad@gmail.com>
2025-08-04 15:08:19 +0300

    add task 1

---
+++ Day-1/Task-1/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Get sum , average for 2 numbers entered by the user
+using System;
+class Task1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first number:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = num1 + num2;
+        double average = sum/2;
+
+        Console.WriteLine($"Sum = {sum}");
+        Console.WriteLine($"Average = {average}");
+    }
+}
