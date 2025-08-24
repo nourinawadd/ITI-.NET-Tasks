b9b4f6ce508a57934c4e791bd1a099a2463b453d
nourinawadd <nourinawad@gmail.com>
2025-08-24 23:35:36 +0300

    init day 13

---
+++ Day-13/Task-1/Task-1/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task_1.Models;
+
+namespace MovieApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // in memory
+        private static readonly List<Movie> _movies = new()
+        {
+            new Movie { Id = 1, Title = "Inception", Director = "Christopher Nolan", ReleaseYear = 2010 },
+            new Movie { Id = 2, Title = "Spirited Away", Director = "Hayao Miyazaki", ReleaseYear = 2001 },
+            new Movie { Id = 3, Title = "Parasite", Director = "Bong Joon-ho", ReleaseYear = 2019 }
+        };
+
+        // index
+        public IActionResult Index()
+        {
+            return View(_movies);
+        }
+
+        // details
+        public IActionResult Details(int id)
+        {
+            var movie = _movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null) return NotFound();
+            return View(movie);
+        }
+    }
+}
---
+++ Day-13/Task-1/Task-1/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Director { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
